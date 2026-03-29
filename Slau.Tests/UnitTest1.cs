819cfe7afb2f587d97c728b79f14f2896add4189
Evklidiy <itsleshaxd@gmail.com>
2026-03-29 14:42:29 +0300

    Добавьте файлы проекта.

---
+++ Slau.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Slau.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
